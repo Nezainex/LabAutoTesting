304047b7640845d80e41e37578ac207bba6d4c9a
Nezainex <111506983+Nezainex@users.noreply.github.com>
2024-11-07 16:38:59 +0300

    cart

--- src/test/java/org/example/locators/CartPageLocators.java
+++ src/test/java/org/example/locators/CartPageLocators.java
@@ -4,24 +4,20 @@ import org.openqa.selenium.By;
 
 public class CartPageLocators {
 
-    // Локатор кнопки добавления продукта в корзину
     public static By getAddToCartButtonByProductName(String productName) {
-        return By.xpath("//a[@data-product-name='" + productName + "' and @title='В корзину']");
+        return By.xpath(".//a[@data-product-name='" + productName + "' and @title='В корзину']");
     }
 
-    // Локатор для получения цены продукта на странице анализа
     public static By getProductPriceLocatorByName(String productName) {
-        return By.xpath("//a[@data-product-name='" + productName + "']/ancestor::div[contains(@class, 'analyzes-item__total')]//div[contains(@class, 'analyzes-item__total--sum')]");
+        return By.xpath(".//a[@data-product-name='" + productName + "']/ancestor::div[contains(@class, 'analyzes-item__total')]//div[contains(@class, 'analyzes-item__total--sum')]");
     }
 
-    // Локатор для значка корзины
     public static final By ICON_CART = By.xpath(".//div[@id='headerCartDynamic']//a//*[name()='svg']");
 
-    // Локатор для очистки корзины (иконка удаления)
-    public static final By CLEAR_CART_BUTTON = By.xpath("//button[contains(@class, 'CartProductsPanel_cartClear') and contains(., 'Очистить корзину')]");
+    public static final By CLEAR_CART_BUTTON = By.xpath(".//button[contains(@class, 'CartProductsPanel_cartClear') and contains(., 'Очистить корзину')]");
 
-    // Локатор для цены продукта в корзине
     public static By getCartProductPriceByName(String productName) {
-        return By.xpath("//button[contains(text(), '" + productName + "')]/ancestor::div[contains(@class, 'CartProduct_product__euiRX')]//div[@class='CartProduct_productPrice__1lW-7']/span");
+        return By.xpath(".//button[contains(text(), '" + productName + "')]/ancestor::div[contains(@class, 'CartProduct_product__euiRX')]//div[@class='CartProduct_productPrice__1lW-7']/span");
     }
 }
+
--- src/test/resources/features/cart.feature
+++ src/test/resources/features/cart.feature
@@ -1,8 +1,8 @@
 Feature: Cart Testing
 
   Scenario: Adding a product to the cart and comparing the total amount
-    Given I remember the price of the product "Glucose (in blood) (Glucose)"
-    When I add the product "Glucose (in blood) (Glucose)" to the cart
+    Given I remember the price of the product "Глюкоза (в крови) (Glucose)"
+    When I add the product "Глюкоза (в крови) (Glucose)" to the cart
     And I open the cart
     Then I compare the total amount in the cart with the remembered price
     And I clear the cart
